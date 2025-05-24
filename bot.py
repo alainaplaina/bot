8180d6b0bae121252823ec847ba1d7128a7ca2ac
Gloriia <Vilochka622@protonmail.com>
2025-05-24 15:02:29 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import pandas as pd
 import os
+import Beautysoulp
 
 print(" ")
 
